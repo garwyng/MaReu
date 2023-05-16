853780dceddac8747641471ed05fc6ac18b0bd68
sebastien cusset <sebastiencusset@hotmail.com>
2023-05-16 19:43:59 +0200

    update 2023/05/16

---
+++ app/src/main/java/com/openclassrooms/mareu/view/AddMeetingActivity.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.mareu.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.openclassrooms.mareu.R;
+
+public class AddMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meeting);
+    }
+}
